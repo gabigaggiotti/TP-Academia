d18cf5f433b51b2659f5cdb8a882f0f93f3cb189
tomaslardizabal <poty.2000.tl@gmail.com>
2023-07-12 19:55:09 -0300

    Agregué la entidad Planes, verificar si va en ese lugar. Preguntar acerca del manejo de excepciones

--- TP Academia/Consola/Program.cs
+++ TP Academia/Consola/Program.cs
@@ -1,1 +1,28 @@
-﻿
+﻿using System.Numerics;
+using System;
+using Entidades;
+public class NotImplementedException : SystemException{ } // PREGUNTARLE AL PROFE QUE ONDA ESTO
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Planes planes = new Planes();
+
+        // Ejemplo de alta de un plan
+        Plan nuevoPlan = new Plan();
+        nuevoPlan.Descripcion = "Plan A";
+        nuevoPlan.IDEspecialidad = 1;
+        planes.AgregarPlan(nuevoPlan);
+
+        // Ejemplo de modificación de un plan
+        //Plan planExistente = // Obtener plan existente de alguna forma (puede ser por búsqueda, selección de lista, etc.)
+        // nuevoPlan.ModificarPlan(planExistente, "Nuevo nombre", 2);
+
+        // Ejemplo de baja de un plan
+        //planes.EliminarPlan(planExistente);
+
+        // Resto de la lógica de tu aplicación...
+        throw new NotImplementedException();
+    }
+}
