204ffacb09243ccf225b86e0dcbe1d4f9b1e1958
tomaslardizabal <poty.2000.tl@gmail.com>
2023-07-12 19:55:32 -0300

    Agregué la entidad Planes, verificar si va en ese lugar. Preguntar acerca del manejo de excepciones

---
+++ TP Academia/Entidades/Planes.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entidades;
+
+public class Planes
+{
+    private List<Plan> _planes;
+
+    public Planes()
+    {
+        _planes = new List<Plan>();
+    }
+
+    public void AgregarPlan(Plan plan)
+    {
+        _planes.Add(plan);
+    }
+
+    public void EliminarPlan(Plan plan)
+    {
+        _planes.Remove(plan);
+    }
+
+    public void ModificarPlan(Plan plan, string nuevaDescripcion, int nuevoIdEspecialidad)
+    {
+        plan.Descripcion = nuevaDescripcion;
+        plan.IDEspecialidad = nuevoIdEspecialidad;
+    }
+
+}
+
---
+++ TP Academia/Planes.cs
@@ -0,0 +1,29 @@
+﻿/*using System;
+
+public class Planes
+{
+    private List<Plan> _planes;
+
+    public Planes()
+    {
+        _planes = new List<Plan>();
+    }
+
+    public void AgregarPlan(Plan plan)
+    {
+        _planes.Add(plan);
+    }
+
+    public void EliminarPlan(Plan plan)
+    {
+        _planes.Remove(plan);
+    }
+
+    public void ModificarPlan(Plan plan, string nuevaDescripcion, int nuevoIdEspecialidad)
+    {
+        plan.Descripcion = nuevaDescripcion;
+        plan.IdEspecialidad = nuevoIdEspecialidad;
+    }
+
+}
+*/
